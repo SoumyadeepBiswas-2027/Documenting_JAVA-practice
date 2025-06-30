d73a4f329372dccb46b4149413514617ce7195d5
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-07-01 00:30:44 +0530

    few more patterns are added

--- pattern.java
+++ pattern.java
@@ -83,19 +83,83 @@
 // }
 // }
 
-// 4) Print the  pyramid pattern of numbers!
+// // 4) Print the  pyramid pattern of numbers!
 
 
-public class pattern {
-public static void main(String[] args) {
+// public class pattern {
+// public static void main(String[] args) {
 
- int n=5;
+//  int n=5;
 
- for(int i=1;i<=n;i++){     
-  for(int j=1;j<=i;j++){
-   System.out.print(j+" ");
-  }                      
-  System.out.println();
- }   
-}
-}
+//  for(int i=1;i<=n;i++){     
+//   for(int j=1;j<=i;j++){
+//    System.out.print(j+" ");
+//   }                      
+//   System.out.println();
+//  }   
+// }
+// }
+
+// 4) Print the  inverted pyramid pattern of numbers!
+
+
+// public class pattern {
+// public static void main(String[] args) {
+
+//  int n=5;
+
+//  for(int i=1 ;i<=n ;i++){     
+//   for(int j=1; j<=n-i+1 ;j++){
+
+//    System.out.print(j+" ");
+//   }                      
+//   System.out.println();
+//  }   
+// }
+// }
+
+ // 4) Print the  pattern of floyd's Triangle!
+
+
+
+// public class pattern {
+// public static void main(String[] args) {
+
+//  int n=5,Sum=1;
+ 
+
+//  for(int i=1 ;i<=n ;i++){     
+//   for(int j=1; j<=i ;j++){
+//    System.out.print(Sum + " ");
+//    Sum++;
+//   }                      
+//   System.out.println();
+//  }   
+// }
+// }
+
+
+// 4) Print the  pattern of half pyramid using 0 and 1 *hint-->if even then 1 else 0
+
+
+
+// public class pattern {
+// public static void main(String[] args) {
+
+//  int n=5;
+ 
+
+//  for(int i=1 ;i<=n ;i++){     
+//   for(int j=1; j<=i ;j++){
+//     int sum=0;
+//     sum=i+j;
+//     if(sum%2==0){
+//       System.out.print("1"+" ");
+//     }else{
+//       System.out.print("0"+" ");
+//     }
+//   }                      
+//   System.out.println();
+//  }   
+// }
+// }
