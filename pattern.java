14ed0ac6c7068da85b4343ace2f77e5c29ebaeb0
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-06-30 23:59:46 +0530

    new pattern added

--- pattern.java
+++ pattern.java
@@ -83,3 +83,19 @@
 // }
 // }
 
+// 4) Print the  pyramid pattern of numbers!
+
+
+public class pattern {
+public static void main(String[] args) {
+
+ int n=5;
+
+ for(int i=1;i<=n;i++){     
+  for(int j=1;j<=i;j++){
+   System.out.print(j+" ");
+  }                      
+  System.out.println();
+ }   
+}
+}
