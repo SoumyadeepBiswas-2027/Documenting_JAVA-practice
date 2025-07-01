97900190c51c5c3ccbecb492c9251b2bf9e97982
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-07-02 00:17:59 +0530

    inserted diamond pattern

--- advPattern.java
+++ advPattern.java
@@ -75,28 +75,63 @@
 
 //4)print palindromic pyramid pattern
 
-public class advPattern {
-    public static void main(String args[]) {
-        int n = 5;
+// public class advPattern {
+//     public static void main(String args[]) {
+//         int n = 5;
 
-        for (int i = 1; i <= n; i++) {
-            // Print spaces
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
+//         for (int i = 1; i <= n; i++) {
+//             // Print spaces
+//             for (int j = 1; j <= n - i; j++) {
+//                 System.out.print(" ");
+//             }
 
-            // Print decreasing numbers
-            for (int j = i; j >= 1; j--) {
-                System.out.print(j);
-            }
+//             // Print decreasing numbers
+//             for (int j = i; j >= 1; j--) {
+//                 System.out.print(j);
+//             }
 
-            // Print increasing numbers (starting from 2 to avoid repeating center digit)
-            for (int j = 2; j <= i; j++) {
-                System.out.print(j);
-            }
+//             // Print increasing numbers (starting from 2 to avoid repeating center digit)
+//             for (int j = 2; j <= i; j++) {
+//                 System.out.print(j);
+//             }
 
-            // Move to next line
-            System.out.println();
-        }
-    }
-}
+//             // Move to next line
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+//5)print diamond pattern 
+// public class advPattern {
+//     public static void main(String args[]) {
+//         int n = 4;
+// //upper half:-
+//         for (int i = 1; i <= n; i++) {
+//             // Print spaces
+//             for (int j = 1; j <= n - i; j++) {
+//                 System.out.print(" ");
+//             }
+//             //stars
+//             for (int j = 1; j <= 2*i-1; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//           }
+//           //lower half:-
+//            for (int i = n; i >= 1; i--) {
+//             // Print spaces
+//             for (int j = 1; j <= n - i; j++) {
+//                 System.out.print(" ");
+//             }
+//             //stars
+//             for (int j = 1; j <= 2*i-1; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//           }
+//         }
+//       }
+
+
