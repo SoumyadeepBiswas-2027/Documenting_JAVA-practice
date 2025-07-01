6efdcc4dc4f1f9ccd28b23d877234d87267e2a70
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-07-01 23:44:45 +0530

    Rhombus pattern added

--- advPattern.java
+++ advPattern.java
@@ -1,37 +1,56 @@
-//1)print butterfly pattern 
-public class advPattern {
-  public static void main(String args[]){
-    int n=5;
-    //upper half
-    for(int i=1;i<=n;i++){
-      for(int j=1;j<=i;j++){
-      System.out.print("*");
-      }
-      int spaces = 2*(n-i);
-       for(int j=1 ;j<=spaces ;j++){
-        System.out.print(" ");
-    } 
-     for(int j=1;j<=i;j++){
-      System.out.print("*");
-      }
-      System.out.println();
+ //1)print butterfly pattern 
+// public class advPattern {
+//   public static void main(String args[]){
+//     int n=5;
+//     //upper half
+//     for(int i=1;i<=n;i++){
+//       for(int j=1;j<=i;j++){
+//       System.out.print("*");
+//       }
+//       int spaces = 2*(n-i);
+//        for(int j=1 ;j<=spaces ;j++){
+//         System.out.print(" ");
+//     } 
+//      for(int j=1;j<=i;j++){
+//       System.out.print("*");
+//       }
+//       System.out.println();
   
-    }  
-    //lower half
+//     }  
+//     //lower half
 
-     for(int i=n; i>=1; i--){
-      for(int j=1;j<=i;j++){
-      System.out.print("*");
-      }
-      int spaces = 2*(n-i);
-       for(int j=1 ;j<=spaces ;j++){
-        System.out.print(" ");
-    } 
-     for(int j=1;j<=i;j++){
-      System.out.print("*");
-      }
-      System.out.println();
+//      for(int i=n; i>=1; i--){
+//       for(int j=1;j<=i;j++){
+//       System.out.print("*");
+//       }
+//       int spaces = 2*(n-i);
+//        for(int j=1 ;j<=spaces ;j++){
+//         System.out.print(" ");
+//     } 
+//      for(int j=1;j<=i;j++){
+//       System.out.print("*");
+//       }
+//       System.out.println();
   
+//     }
+//   }
+// }
+
+
+ //2)print solid rhombus pattern 
+ public class advPattern {
+   public static void main(String args[]){
+    int n=5;
+
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+      }
+      for(int j=1;j<=5;j++){
+       System.out.print("*");
+      }
+       System.out.println();
     }
+   
   }
 }
