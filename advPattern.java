f13fdc8fa8acff63a297b62a07f54ab62b8b61d5
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-07-02 00:04:45 +0530

    palindromic pyramid pattern is added

--- advPattern.java
+++ advPattern.java
@@ -55,20 +55,48 @@
 //   }
 // }
 
-//3)print pyramid pattern  of numbers
-public class advPattern {
-   public static void main(String args[]){
-    int n=5;
+// //3)print pyramid pattern  of numbers
+// public class advPattern {
+//    public static void main(String args[]){
+//     int n=5;
 
-    for(int i=1;i<=n;i++){
-    for(int j=1;j<=n-i;j++){
-      System.out.print(" ");
+//     for(int i=1;i<=n;i++){
+//     for(int j=1;j<=n-i;j++){
+//       System.out.print(" ");
+//     }
+//     //numbers
+//      for(int j=1;j<=i;j++){
+//       System.out.print(i + " ");
+//      }
+//       System.out.println();
+//     }
+//     }
+//   }
+
+//4)print palindromic pyramid pattern
+
+public class advPattern {
+    public static void main(String args[]) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print decreasing numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            // Print increasing numbers (starting from 2 to avoid repeating center digit)
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            // Move to next line
+            System.out.println();
+        }
     }
-    //numbers
-     for(int j=1;j<=i;j++){
-      System.out.print(i + " ");
-     }
-      System.out.println();
-    }
-    }
-  }
+}
