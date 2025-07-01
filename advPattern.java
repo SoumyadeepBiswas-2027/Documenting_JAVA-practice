c49fd6517d35bb760b6293eb511beda1103f10c7
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-07-01 23:53:49 +0530

    inserting pattern of number pyramid

--- advPattern.java
+++ advPattern.java
@@ -38,19 +38,37 @@
 
 
  //2)print solid rhombus pattern 
- public class advPattern {
+//  public class advPattern {
+//    public static void main(String args[]){
+//     int n=5;
+
+//     for(int i=1;i<=n;i++){
+//       for(int j=1;j<=n-i;j++){
+//         System.out.print(" ");
+//       }
+//       for(int j=1;j<=5;j++){
+//        System.out.print("*");
+//       }
+//        System.out.println();
+//     }
+   
+//   }
+// }
+
+//3)print pyramid pattern  of numbers
+public class advPattern {
    public static void main(String args[]){
     int n=5;
 
     for(int i=1;i<=n;i++){
-      for(int j=1;j<=n-i;j++){
-        System.out.print(" ");
-      }
-      for(int j=1;j<=5;j++){
-       System.out.print("*");
-      }
-       System.out.println();
+    for(int j=1;j<=n-i;j++){
+      System.out.print(" ");
     }
-   
+    //numbers
+     for(int j=1;j<=i;j++){
+      System.out.print(i + " ");
+     }
+      System.out.println();
+    }
+    }
   }
-}
