961ac5d01a137ed927d899a3b88d8d625fba9c21
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-08-06 00:18:32 +0530

    Added selection sort

--- DSA-series/Sorting.java
+++ DSA-series/Sorting.java
@@ -1,35 +1,83 @@
 //BUBBLE SORT
 
+// import java.util.Scanner;
+
+// public class Sorting {
+//   public static void PrintArray(int arr[]){
+// for(int i=0;i<=arr.length-1;i++){
+//       System.out.print(arr[i]+" ");
+//   }
+//   }
+//   public static void main(String[] args) {
+//  Scanner sc = new Scanner(System.in);
+
+//   System.out.println("Enter size:");
+//   int size = sc.nextInt();
+//   int arr[] = new int[size];
+
+
+//   System.out.println("Give inputs:");
+// for(int i=0;i<size;i++){
+//   arr[i] = sc.nextInt();
+// }
+
+//       for(int i=0;i<=arr.length-1;i++){
+//         for(int j=0;j<arr.length-i-1;j++){
+//           if(arr[j] > arr[j+1]){
+//             int temp = arr[j];
+//             arr[j]= arr[j+1];
+//             arr[j+1]=temp;
+//           }
+//         }
+//       }
+//      PrintArray(arr);  
+// }
+// }
+
+
 import java.util.Scanner;
 
 public class Sorting {
-  public static void PrintArray(int arr[]){
-for(int i=0;i<=arr.length-1;i++){
-      System.out.print(arr[i]+" ");
-  }
-  }
-  public static void main(String[] args) {
- Scanner sc = new Scanner(System.in);
-
-  System.out.println("Enter size:");
-  int size = sc.nextInt();
-  int arr[] = new int[size];
-
-
-  System.out.println("Give inputs:");
-for(int i=0;i<size;i++){
-  arr[i] = sc.nextInt();
-}
-
-      for(int i=0;i<=arr.length-1;i++){
-        for(int j=0;j<arr.length-i-1;j++){
-          if(arr[j] > arr[j+1]){
-            int temp = arr[j];
-            arr[j]= arr[j+1];
-            arr[j+1]=temp;
-          }
+    public static void PrintArray(int arr[]) {
+        for (int i = 0; i <= arr.length - 1; i++) {
+            System.out.print(arr[i] + " ");
         }
-      }
-     PrintArray(arr);  
+        System.out.println(); // For clean output
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size:");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        System.out.println("Give inputs:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Selection Sort logic
+        for (int i = 0; i < size - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            // Swap only if needed
+            if (min != i) {
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+
+            
+        }
+
+        System.out.println("Sorted array:");
+        PrintArray(arr);
+    }
 }
-}
+
